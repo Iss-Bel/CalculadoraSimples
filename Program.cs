e87af6b338f4846692aa97667f4b1d1f28ade6fd
Bella <isabella.s.silva7@edu.senai.br>
2026-04-15 13:52:41 -0300

    feat: criação inicial da calculadora

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CalculadoraSimples
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine("---CALCULADORA SENAI---");
+            
+            Console.Write("Digite o prímeiro numero: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o segundo número: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double soma = num1 + num2;
+            Console.WriteLine($"O resultado da soma é {soma}");
+        }
+    }
+}
